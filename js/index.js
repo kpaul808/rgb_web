d425ff95742dd79a5325fc95e64afe7d54eb6362
kpaul808 <kainoap808@gmail.com>
2016-09-25 17:44:16 -1000

    got sliders working

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ var blue;
 
 window.onload=colorchange1	
 
-
-
 function colorchange1(){
 	red = Math.floor(Math.random() * 256);
 	console.log(red)
@@ -31,4 +29,26 @@ function CHANGECOLOR(){
 
 function changetitle(){
 	document.getElementById("title").innerHTML= "rgb("+ red+ ", "+green+ ", "+ blue+")"
+}
+
+function changeRed(){
+	document.getElementById("red").innerHTML = "Red is "+document.getElementById("Redslide").value
+	console.log("Redval="+document.getElementById("Redslide").value)
+	changediv()
+}
+
+function changeGreen(){
+	document.getElementById("green").innerHTML = "Green is "+document.getElementById("Greenslide").value
+	console.log("Greenval="+document.getElementById("Greenslide").value)
+	changediv()
+}
+
+function changeBlue(){
+	document.getElementById("blue").innerHTML = "Blue is "+document.getElementById("Blueslide").value
+	console.log("Blueval="+document.getElementById("Blueslide").value)
+	changediv()
+}
+
+function changediv(){
+	document.getElementById('slidecolor').style.backgroundColor= "rgb("+ document.getElementById("Redslide").value+ ", "+document.getElementById("Greenslide").value+ ", "+ document.getElementById("Blueslide").value+")"
 }
